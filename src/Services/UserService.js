22cb4c9fbcf33d71b83a13b2e52375af3a4bffc7
intern.lhdang2 <danglevt1234@gmail.com>
2023-08-23 17:07:47 +0700

    this is front-end(ongoing)

--- src/Services/UserService.js
+++ src/Services/UserService.js
@@ -5,17 +5,21 @@ const USER_API = "http://localhost:8080/users/";
 class UserService {
     getAll() {
         const token= localStorage.getItem("Authorization");
+        console.log(token);
         return axios.get(USER_API + "index",{
             headers:{
-                Authorization:'Bearer '+token
+                'Authorization':'Bearer '+token,
+                'Content-Type':'application/json'
             }
-        })}
+        }
+        )}
 
     getOneById(id) {
         const token= localStorage.getItem("Authorization");
         return axios.get(USER_API + id,{
             headers:{
-                Authorization:'Bearer '+token
+                'Authorization':'Bearer '+token,
+                'Content-Type':'application/json'
             }
         });
     }
@@ -24,7 +28,8 @@ class UserService {
         const token= localStorage.getItem("Authorization");
         return axios.put(USER_API + id, user,{
             headers:{
-                Authorization:'Bearer '+token
+                'Authorization':'Bearer '+token,
+                'Content-Type':'application/json'
             }
         })
     }
@@ -33,7 +38,8 @@ class UserService {
         const token= localStorage.getItem("Authorization");
         return axios.delete(USER_API + id,{
             headers:{
-                Authorization:'Bearer '+token
+                'Authorization':'Bearer '+token,
+                'Content-Type':'application/json'
             }
         });
     }
